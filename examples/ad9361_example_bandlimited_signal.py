a48732ce1c4a6d49e447688998dad9118a175310
Cyber-G <cygerpham@free.fr>
2023-08-05 02:44:10 +0200

    ad9361_example_bandlimited_signal.py: define functions compute_fir_kaiser generate_filtered_noise

--- examples/ad9361_example_bandlimited_signal.py
+++ examples/ad9361_example_bandlimited_signal.py
@@ -11,6 +11,78 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 
+def compute_fir_kaiser(fpass,fstop,fs,atten_level=60):
+    """
+    Compute the coefficients of a Kaiser FIR filter
+
+    Parameters
+    ----------
+    fpass : float
+        Passband frequency [Hz] 
+    fstop : float
+        Stopband frequency [Hz]
+    fs : float
+        Sampling frequency [Hz]
+    atten_level : float, optional
+        Attenuation level [dB]. 
+        The default is 60.
+
+    Returns
+    -------
+    taps : numpy array
+        Filter coefficients
+
+    Author: Germain Pham, @cyber-g, Aug. 2023
+    Inspired by: 
+    https://github.com/unpingco/Python-for-Signal-Processing/blob/master/Filtering_Part2.ipynb
+    """
+    
+    # Compute the filter order
+    M,beta= signal.fir_filter_design.kaiserord(atten_level, (fstop-fpass)/(fs/2.))
+    
+    # Design the filter
+    taps = signal.firwin(M,(fstop+fpass)/2.,window=('kaiser',beta),nyq=fs/2.)
+    
+    return taps
+
+def generate_filtered_noise(size_array, BW, FS):
+    """
+    Generate a filtered noise signal
+
+    Parameters
+    ----------
+    size_array : tuple of int
+        matrix size
+    BW : float
+        Bandwidth of the signal [Hz]
+    FS : float
+        Sampling frequency      [Hz]
+
+    Returns
+    -------
+    noise_sig : numpy array
+        Filtered noise signal
+
+    Author: Germain Pham, @cyber-g, Aug. 2023
+    """
+    # Generate the noise
+    noise = np.random.normal(size=size_array)
+    
+    # Instanciate the filter
+    taps = compute_fir_kaiser(0, BW, FS)
+
+    # Filter the noise, use convolve in order to get transients fade in/out
+    # making the signal more circular (useful for spectral analysis and cyclic
+    # transmission)
+    noise_sig = signal.convolve(taps, noise)
+
+    # Normalize the signal to have unitary magnitude
+    noise_sig = noise_sig/np.max(np.abs(noise_sig))
+    
+    return noise_sig
+
+
+
 # Create radio
 sdr = adi.ad9361(uri="ip:analog.local")
 
