8fdc5c7a84726c8d2d9f893f84686d01582a7f00
Cyber-G <cygerpham@free.fr>
2023-08-05 02:34:18 +0200

    ad9361_example_bandlimited_signal.py: refactor for code readability

--- examples/ad9361_example_bandlimited_signal.py
+++ examples/ad9361_example_bandlimited_signal.py
@@ -15,12 +15,12 @@ from scipy import signal
 sdr = adi.ad9361(uri="ip:analog.local")
 
 # Configure properties
-sdr.rx_rf_bandwidth = 4000000
-sdr.sample_rate = 6000000
-sdr.rx_lo = 2000000000
-sdr.tx_lo = 2000000000
-sdr.tx_cyclic_buffer = True
-sdr.tx_hardwaregain_chan0 = -30
+sdr.rx_rf_bandwidth         = int(4e6)
+sdr.sample_rate             = int(6e6)
+sdr.rx_lo                   = int(2e9)
+sdr.tx_lo                   = int(2e9)
+sdr.tx_cyclic_buffer        = True
+sdr.tx_hardwaregain_chan0   = -30
 sdr.gain_control_mode_chan0 = "slow_attack"
 
 # Configuration data channels
@@ -31,13 +31,16 @@ sdr.tx_enabled_channels = [0]
 print("RX LO %s" % (sdr.rx_lo))
 
 # Create a sinewave waveform
-fs = int(sdr.sample_rate)
-N = 1024
-fc = int(1000000 / (fs / N)) * (fs / N)
-ts = 1 / float(fs)
-t = np.arange(0, N * ts, ts)
-i = np.cos(2 * np.pi * t * fc) * 2 ** 14
-q = np.sin(2 * np.pi * t * fc) * 2 ** 14
+fs    = int(sdr.sample_rate)
+N     = 1024
+fc    = int(1e6 / (fs / N)) * (fs / N)
+ts    = 1 / float(fs)
+t     = np.arange(0, N * ts, ts)
+
+DAC_full_scale_int = 2 ** 14
+
+i = np.cos(2 * np.pi * t * fc) * DAC_full_scale_int
+q = np.sin(2 * np.pi * t * fc) * DAC_full_scale_int
 iq = i + 1j * q
 
 # Send data
